b609d1905a2e97cef85972a4e2ef951a13f0560e
szymonpoterejkoinfe2 <sp306077@student.polsl.pl>
2024-12-20 18:09:15 +0100

    Feature: Validation rule for textboxes

---
+++ ViewModel/Validators/NumericValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PhotoAdder.ViewModel.Validators
+{
+    public class NumericValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            //throw new NotImplementedException();
+
+            string numericRule = @"^-?\d+$";
+            Regex numericRegex = new Regex(numericRule);
+
+            if (!numericRegex.IsMatch(value.ToString()))
+            {
+                return new ValidationResult(false, "Please enter numeric value");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
