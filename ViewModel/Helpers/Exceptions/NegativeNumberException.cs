55cfce79d8792a4a277d091139c50a7ae4d1b086
szymonpoterejkoinfe2 <sp306077@student.polsl.pl>
2024-12-22 18:16:06 +0100

    Fix: ExcelReaderHelper fixes after unit testing, custom exceptions

---
+++ ViewModel/Helpers/Exceptions/NegativeNumberException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAdder.ViewModel.Helpers.Exceptions
+{
+    public class NegativeNumberException : Exception
+    {
+        public NegativeNumberException() { }
+
+        int? NegativeNumber { get; set; }
+        public NegativeNumberException(int? number) : base($"Error occured while trying to operate on negative number:{number}! Please enter valid positive integer number") { NegativeNumber = number; }
+    }
+}
