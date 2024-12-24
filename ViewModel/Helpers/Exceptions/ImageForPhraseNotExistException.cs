6279ebe0cbe18739f941b85f4fde652ca1748458
szymonpoterejkoinfe2 <sp306077@student.polsl.pl>
2024-12-24 12:25:00 +0100

    Fix: WebScraperHelper fixes after unit testing, custom exceptions

---
+++ ViewModel/Helpers/Exceptions/ImageForPhraseNotExistException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAdder.ViewModel.Helpers.Exceptions
+{
+    public class ImageForPhraseNotExistException : Exception
+    {
+        public ImageForPhraseNotExistException(string phrase) : base($"Image for given phrase: {phrase} not found!")
+        {
+        }
+
+    }
+}
