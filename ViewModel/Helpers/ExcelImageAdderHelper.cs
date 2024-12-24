c1003c1f0b02c093a4f6e5cce5a9fdc866b1a299
szymonpoterejkoinfe2 <sp306077@student.polsl.pl>
2024-12-24 14:23:07 +0100

    Fix: ExcelImageAdderHelper fixes after unit testing, custom exceptions

--- ViewModel/Helpers/ExcelImageAdderHelper.cs
+++ ViewModel/Helpers/ExcelImageAdderHelper.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using PhotoAdder.Model.Classes;
+using PhotoAdder.ViewModel.Helpers.Exceptions;
+using System.Linq;
 
 namespace PhotoAdder.ViewModel.Helpers
 {
@@ -14,18 +16,36 @@ namespace PhotoAdder.ViewModel.Helpers
         public string? _ImageFilePath = null;
         public void AddImageToExcelFile(int worksheetPage, int worksheetRow, int worksheetImageCell)
         {
-            if (File.Exists(_ImageFilePath))
+
+            if (File.Exists(_ImageFilePath) && _ImageFilePath != null)
             {
+                if (!validExtension(_ImageFilePath))
+                {
+                    throw new InvalidFileExtensionException(_ImageFilePath);
+                }
+
+                if (!File.Exists(AppData.ExcelFile))
+                {
+                    throw new ExcelFileNotExistsException(AppData.ExcelFile);
+                }
+
+
                 using (var workbook = new XLWorkbook(AppData.ExcelFile))
                 {
+                    if (workbook.Worksheets.Count < worksheetPage || worksheetPage < 1)
+                    {
+                        throw new ExcelWorksheetNotExistException(worksheetPage);
+                    }
+
                     var worksheet = workbook.Worksheet(worksheetPage);
-
                     var row = worksheet.Row(worksheetRow);
-
                     var imageCell = row.Cell(worksheetImageCell);
+
                     var picture = worksheet.AddPicture(_ImageFilePath).MoveTo(imageCell).WithSize(100, 100);
+
                     worksheet.Column(imageCell.Address.ColumnNumber).Width = 15;
                     worksheet.Row(worksheetRow).Height = 75;
+                    
                     workbook.Save();
 
                     Thread.Sleep(200);
@@ -33,9 +53,19 @@ namespace PhotoAdder.ViewModel.Helpers
 
             }
             else {
-                System.Windows.Forms.MessageBox.Show($"Image not found:", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw new InvalidImageFilePathException(_ImageFilePath);
             }
 
         }
+
+        private bool validExtension(string filePath)
+        {
+            string extension = Path.GetExtension(filePath)?.ToLower();
+            string[] validExtensions = { ".jpg", ".jpeg", ".png"};
+
+            return validExtensions.Contains(extension);
+        }
+
+
     }
 }
---
+++ ViewModel/Helpers/Exceptions/InvalidImageFilePathException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAdder.ViewModel.Helpers.Exceptions
+{
+    public class InvalidImageFilePathException : Exception
+    {
+        public InvalidImageFilePathException(string? filePath): base($"Invalid image file path: {filePath ?? "Unknown"}")
+        {
+        }
+    }
+}
