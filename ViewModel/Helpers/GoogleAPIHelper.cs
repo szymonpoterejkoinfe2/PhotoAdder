0faa5366177d762c3fa30cd4f5bb537f61606798
szymonpoterejkoinfe2 <sp306077@student.polsl.pl>
2025-01-07 14:39:39 +0100

    Fix: GoogleAPIHelper fixes after unit tests, custom exceptions

--- ViewModel/Helpers/GoogleAPIHelper.cs
+++ ViewModel/Helpers/GoogleAPIHelper.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
 using PhotoAdder.Model.Classes;
+using PhotoAdder.ViewModel.Helpers.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,31 +17,40 @@ namespace PhotoAdder.ViewModel.Helpers
     {
         private readonly string SEARCH_ENGINE_ID = ApiData.SearchEngineId;
         private readonly string API_KEY = ApiData.ApiKey;
-        
-        public async Task<string> GetURLAsync(string searchPhrase)
-        {
 
-            return await GetFirstImageAsync(searchPhrase);
-
-        }
-        private  async Task<string> GetFirstImageAsync(string query)
+        public async Task<string> GetURLAsync(string? searchPhrase)
         {
-          
+            if (string.IsNullOrEmpty(API_KEY) || API_KEY == "API_KEY")
+            {
+                throw new ApiKeyNotSetException();
+            }
+
+            if (string.IsNullOrEmpty(SEARCH_ENGINE_ID) || SEARCH_ENGINE_ID == "SEARCH_ENGINE_ID")
+            {
+                throw new SearchEngineIdNotSetException();
+            }
+                
+            if (string.IsNullOrEmpty(searchPhrase) || string.IsNullOrWhiteSpace(searchPhrase))
+            {
+                throw new NullOrEmptySearchPhraseException();
+            }
+
+
             using (HttpClient client = new HttpClient())
             {
-                string encodedQuery = Uri.EscapeDataString(query);
+                string encodedQuery = Uri.EscapeDataString(searchPhrase);
                 string requestUrl = $"https://www.googleapis.com/customsearch/v1?q={encodedQuery}&key={API_KEY}&cx={SEARCH_ENGINE_ID}&num=1";
                 string? imageUrl = null;
 
-                try
+                //Make HTTP request
+                var response = await client.GetAsync(requestUrl);
+                //response.EnsureSuccessStatusCode();
+
+                string content = await response.Content.ReadAsStringAsync();
+                JObject? jsonResponse = JObject.Parse(content);
+
+                if (jsonResponse["items"] != null && jsonResponse["items"].ToArray().Length > 0)
                 {
-                    // Wykonaj żądanie HTTP
-                    var response = await client.GetAsync(requestUrl);
-                    //response.EnsureSuccessStatusCode();
-
-                    string content = await response.Content.ReadAsStringAsync();
-                    JObject? jsonResponse = JObject.Parse(content);
-
                     foreach (var item in jsonResponse["items"])
                     {
                         // Attempt to get the first image URL (from cse_image)
@@ -49,25 +60,22 @@ namespace PhotoAdder.ViewModel.Helpers
                             // Fallback to cse_thumbnail if no cse_image exists
                             imageUrl = item["pagemap"]?["cse_thumbnail"]?[0]?["src"]?.ToString();
                         }
+                    }
 
+                    if (string.IsNullOrEmpty(imageUrl))
+                    {
+                        throw new UrlExtractionException();
                     }
                 }
-                catch (Exception ex)
+                else
                 {
-                    Console.WriteLine($"Błąd podczas pobierania obrazu dla frazy '{query}': {ex.Message}");
-                    MessageBox.Show($"Image not found:'{query}', {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    throw new UrlExtractionException();
                 }
 
                 return imageUrl;
             }
 
-
-            //return imageUrls[random.Next(0, 3)];
-
-
         }
     }
-
-
 
 }
